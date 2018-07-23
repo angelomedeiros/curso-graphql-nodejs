3fd5eea29c10d779de9a1fa2f3f1932b79e4b53e
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-23 16:32:52 -0300

    Removendo o gulp-livereoload

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,17 @@ const gulp    = require('gulp')
 const clean   = require('gulp-clean')
 const ts      = require('gulp-typescript')
 const nodemon = require('gulp-nodemon')
-const livereload   = require('gulp-livereload')
 
 const tsProject = ts.createProject('tsconfig.json')
 
 let initServer = () => {
   process.env.NODE_ENV = 'development'
-  livereload.listen()
   nodemon({
     script: 'dist/',
     ext: 'js'
   })
   .on('restart', () => {
     gulp.src('dist/')
-      .pipe(livereload())
   })
 }
 
@@ -27,13 +24,11 @@ gulp.task('scripts', ['static'], () => {
 gulp.task('static', ['clean'], () => {
   return gulp.src(['src/**/*.json'])
              .pipe(gulp.dest('dist'))
-             .pipe(livereload())
 })
 
 gulp.task('clean', () => {
   return gulp.src('dist/*')
              .pipe(clean())
-             .pipe(livereload())
 })
 
 gulp.task('build', ['clean', 'static', 'scripts'])
