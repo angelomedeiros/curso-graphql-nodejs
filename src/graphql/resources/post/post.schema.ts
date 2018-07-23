395462cf43a36aa70ba89d736589b788b8f70c71
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-23 18:13:33 -0300

    Implementando o post.schema

---
+++ src/graphql/resources/post/post.schema.ts
@@ -0,0 +1,38 @@
+const postTypes = `
+
+    # Post definitation types
+    type Post {
+        id: ID!
+        title: String!
+        content: String!
+        photo: String!
+        createdAt: String!
+        updatedAt: String!
+        author: User!
+        comments: [ Comments! ]!
+    }
+
+    input PostInput {
+        title: String!
+        content: String!
+        photo: String!
+        author: Int!
+    }
+`
+
+const postQueries = `
+    posts(first: Int, offset: Int): [ Post! ]!
+    post(id: ID!): Post
+`
+
+const postMutations = `
+    createPost(input: PostInput!): Post
+    updatePost(id: ID!, input: PostInput!): Post
+    deletePost(id: ID!): Boolean
+`
+
+export {
+    postTypes,
+    postQueries,
+    postMutations
+}
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,7 +1,10 @@
 import { makeExecutableSchema } from 'graphql-tools'
-import { Query } from './query'
-import { Mutation } from './mutation'
-import { userTypes } from './resources/user/user.schema';
+
+import { Query }                from './query'
+import { Mutation }             from './mutation'
+
+import { postTypes }            from './resources/post/post.schema'
+import { userTypes }            from './resources/user/user.schema'
 
 const SchemaDefinition = `
     type Schema {
@@ -15,6 +18,7 @@ export default makeExecutableSchema({
         SchemaDefinition,
         Query,
         Mutation,
-        userTypes
+        userTypes,
+        postTypes
     ]
 })
