35c39eb4031f443aa43e3eab49211859b9a3f1d5
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-24 19:12:34 -0300

    Implementando o UserLoader

---
+++ src/graphql/dataloaders/UserLoader.ts
@@ -0,0 +1,14 @@
+import { IUserInstance, IUserModel } from '../../models/MUser'
+import * as Bluebird from 'bluebird'
+
+export class UserLoader {
+    
+    static batchUsers(User: IUserModel, ids: number[]): Bluebird<IUserInstance[]> {
+        return  Bluebird.resolve(
+            User.findAll({
+                where: { id: { $in: ids } }
+            })
+        )
+    }
+
+}
