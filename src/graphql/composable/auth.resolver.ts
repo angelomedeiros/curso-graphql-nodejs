6dfafbd88d37ccc7917503e4c2de580584778f05
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-07-24 15:36:08 -0300

    Implementando o auth.resolver

---
+++ src/graphql/composable/auth.resolver.ts
@@ -0,0 +1,18 @@
+import { GraphQLFieldResolver } from "graphql";
+import { IResolverContext }     from '../../interfaces/IResolverContext'
+import { ComposableResolver }   from './composable.resolver'
+
+export const authResolver: ComposableResolver<any, IResolverContext> = 
+    (resolver: GraphQLFieldResolver<any, IResolverContext>): GraphQLFieldResolver<any, IResolverContext> => {
+        
+        return (parent, args, context: IResolverContext, info) => {
+
+            if (context.user || context.authorization) {
+                return resolver(parent, args, context, info)
+            }
+
+            throw new Error('Não autorizado: Token não fornecido')
+
+        }
+
+    }
